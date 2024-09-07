74d4feaad743bb600a0e40e03213601e005a0590
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-09-07 23:30:10 +0700

    Create Investor Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Investor(models.Model):
+    """Investor Model represents the investor, containing their name and contacts."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(blank=False, null=False)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name + ' ' + self.last_name}"
