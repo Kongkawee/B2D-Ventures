a4030a02adb448aaf5afa5b3be0a7b1a67c0b227
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-09-09 19:13:15 +0700

    Re design Sign in and Sign up page and modify path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
 import react from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Home from "./pages/Home"
+import Home from "./pages/home-page/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
+import SignUp from "./pages/sign-up-page/SignUp"
+import SignInSide from "./pages/sign-in-page/SignInSide"
+// import BusinessInfo from "./pages/business-info-page/Business"
 
 function Logout() {
   localStorage.clear()
-  return <Navigate to="/login" />
+  return <Navigate to="/sin" />
 }
 
 function RegisterAndLogout() {
   localStorage.clear()
-  return <Register />
+  return <SignUp />
 }
 
 function App() {
@@ -21,9 +22,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="/sin" element={<SignInSide />} /> Sign in page
+        <Route path="/logout" element={<Logout />} /> 
+        <Route path="/sup" element={<RegisterAndLogout />} /> Sign up Page
+
+        {/* <Route path="/bus" element={<BusinessInfo />} /> Business info Page */}
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/sign-in-page/Content.jsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import AutoFixHighRoundedIcon from '@mui/icons-material/AutoFixHighRounded';
+import ConstructionRoundedIcon from '@mui/icons-material/ConstructionRounded';
+import SettingsSuggestRoundedIcon from '@mui/icons-material/SettingsSuggestRounded';
+import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded';
+
+import LogoLight from "../../images/LogoLight.png";
+import LogoDark from "../../images/LogoDark.png";
+
+const items = [
+  {
+    icon: <SettingsSuggestRoundedIcon sx={{ color: 'text.secondary' }} />,
+    title: 'Adaptable performance',
+    description:
+      'Our product effortlessly adjusts to your needs, boosting efficiency and simplifying your tasks.',
+  },
+  {
+    icon: <ConstructionRoundedIcon sx={{ color: 'text.secondary' }} />,
+    title: 'Built to last',
+    description:
+      'Experience unmatched durability that goes above and beyond with lasting investment.',
+  },
+  {
+    icon: <ThumbUpAltRoundedIcon sx={{ color: 'text.secondary' }} />,
+    title: 'Great user experience',
+    description:
+      'Integrate our product into your routine with an intuitive and easy-to-use interface.',
+  },
+  {
+    icon: <AutoFixHighRoundedIcon sx={{ color: 'text.secondary' }} />,
+    title: 'Innovative functionality',
+    description:
+      'Stay ahead with features that set new standards, addressing your evolving needs better than the rest.',
+  },
+];
+
+const logoStyle = {
+  width: "100px",
+  margin: '10px',
+  height: "auto",
+  cursor: "pointer",
+};
+
+export default function Content(mode) {
+  return (
+    <Stack
+      sx={{ flexDirection: 'column', alignSelf: 'center', gap: 4, maxWidth: 450 }}
+    >
+      <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+        <img
+          src={mode === "light" ? LogoLight : LogoDark}
+          style={logoStyle}
+          alt="logo of b2d"
+        />
+      </Box>
+      {items.map((item, index) => (
+        <Stack key={index} direction="row" sx={{ gap: 2 }}>
+          {item.icon}
+          <div>
+            <Typography gutterBottom sx={{ fontWeight: 'medium' }}>
+              {item.title}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              {item.description}
+            </Typography>
+          </div>
+        </Stack>
+      ))}
+    </Stack>
+  );
+}
