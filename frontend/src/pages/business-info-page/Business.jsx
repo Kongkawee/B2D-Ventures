8e80b72dc55770f1c4d29a9d350e63e2efd6b1b4
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-09-16 20:18:54 +0700

    Debug Business Missing Files

--- frontend/src/pages/business-info-page/Business.jsx
+++ frontend/src/pages/business-info-page/Business.jsx
@@ -1,39 +1,36 @@
-import * as React from 'react';
-import { createTheme, ThemeProvider, alpha } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import getDashboardTheme from './theme/getDashboardTheme';
-import AppNavbar from './components/AppNavbar';
-import Header from './components/Header';
-import MainGrid from './components/MainGrid';
-import TemplateFrame from './TemplateFrame';
-import ScrollToTopButton from '../../components/ToTopButton';
+import * as React from "react";
+import { createTheme, ThemeProvider, alpha } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import getDashboardTheme from "./theme/getDashboardTheme";
+import MainGrid from "./components/MainGrid";
+import TemplateFrame from "./TemplateFrame";
 
 export default function BusinessInfo() {
-  const [mode, setMode] = React.useState('light');
+  const [mode, setMode] = React.useState("light");
   const [showCustomTheme, setShowCustomTheme] = React.useState(true);
   const dashboardTheme = createTheme(getDashboardTheme(mode));
   const defaultTheme = createTheme({ palette: { mode } });
   // This code only runs on the client side, to determine the system color preference
   React.useEffect(() => {
     // Check if there is a preferred mode in localStorage
-    const savedMode = localStorage.getItem('themeMode');
+    const savedMode = localStorage.getItem("themeMode");
     if (savedMode) {
       setMode(savedMode);
     } else {
       // If no preference is found, it uses system preference
       const systemPrefersDark = window.matchMedia(
-        '(prefers-color-scheme: dark)',
+        "(prefers-color-scheme: dark)"
       ).matches;
-      setMode(systemPrefersDark ? 'dark' : 'light');
+      setMode(systemPrefersDark ? "dark" : "light");
     }
   }, []);
 
   const toggleColorMode = () => {
-    const newMode = mode === 'dark' ? 'light' : 'dark';
+    const newMode = mode === "dark" ? "light" : "dark";
     setMode(newMode);
-    localStorage.setItem('themeMode', newMode); // Save the selected mode to localStorage
+    localStorage.setItem("themeMode", newMode); // Save the selected mode to localStorage
   };
 
   const toggleCustomTheme = () => {
@@ -49,20 +46,20 @@ export default function BusinessInfo() {
     >
       <ThemeProvider theme={showCustomTheme ? dashboardTheme : defaultTheme}>
         <CssBaseline enableColorScheme />
-        <Box sx={{ display: 'flex', p:4 }}>
+        <Box sx={{ display: "flex", p: 4 }}>
           {/* Main content */}
           <Box
             component="main"
             sx={(theme) => ({
               flexGrow: 1,
               backgroundColor: alpha(theme.palette.background.default, 1),
-              overflow: 'auto',
+              overflow: "auto",
             })}
           >
             <Stack
               spacing={2}
               sx={{
-                alignItems: 'center',
+                alignItems: "center",
                 mx: 3,
                 pb: 10,
                 mt: { xs: 8, md: 0 },
