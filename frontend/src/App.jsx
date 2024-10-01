22ebd4d6842ef9fcd1b8761fb238ccd5369ed6d7
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-10-01 09:26:47 +0700

    Update path to Exploration Page and Fix Home Page Component Name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import react from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Home from "./pages/home-page/Home"
+import HomePage from "./pages/home-page/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
 import SignUp from "./pages/sign-up-page/SignUp"
 import SignInSide from "./pages/sign-in-page/SignInSide"
 import Checkout from "./pages/checkout-page/Checkout"
 import BusinessInfo from "./pages/business-info-page/Business"
+// import BusinessExplorationPage from "./pages/business-exploration-page/BusinessExploration"
 
 function Logout() {
   localStorage.clear()
@@ -22,12 +23,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<HomePage />}/>
         <Route path="/sin" element={<SignInSide />} /> Sign in page
         <Route path="/logout" element={<Logout />} /> 
         <Route path="/sup" element={<RegisterAndLogout />} /> Sign up Page
         <Route path="/checkout" element={<Checkout/>} /> Checkout
         <Route path="/bus" element={<BusinessInfo />} /> Business info Page
+        {/* <Route path="/explore" element={<BusinessExplorationPage />} /> Business exploration Page */}
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
