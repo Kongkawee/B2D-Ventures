e9eed568839fd3a4efe9b163dd14e666518bf3dd
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-10-02 09:40:48 +0700

    add login view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Investor
@@ -22,3 +23,16 @@ def register_investor(request):
     
     refresh = RefreshToken.for_user(user)
     return Response({'refresh': str(refresh), 'access': str(refresh.access_token)})
+
+
+@api_view(['POST'])
+def login_investor(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        refresh = RefreshToken.for_user(user)
+        return Response({'refresh': str(refresh), 'access': str(refresh.access_token)})
+    else:
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
