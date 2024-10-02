1a33301b5ab3e85ada4c1527c63936b614e7ddea
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-10-02 11:21:27 +0700

    update investor model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 class Investor(models.Model):
     """Investor Model represents the investor, containing their name and contacts."""
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(blank=False, null=False)
+    email = models.EmailField(blank=False, null=False, unique=True)
     phone_number = models.CharField(max_length=10, blank=True, null=True)
 
     def __str__(self):
-        return f"{self.first_name + ' ' + self.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
 
 class Business(models.Model):
