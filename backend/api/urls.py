8ec6835e35b7317f6aec5ffe18e5ac7fef9a952e
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-10-10 07:10:49 +0700

    Add some paths

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
-from .views import register_investor, login_investor, register_business, login_business, invest, ListInvestor, DetailInvestor
+from .views import (
+    register_investor, 
+    login_investor, 
+    register_business, 
+    login_business,
+    invest,
+    ListInvestor, 
+    DetailInvestor,
+    ListBusiness,
+    DetailBusiness,
+    ListInvestment,
+    DetailInvestment,
+)
 
 urlpatterns = [
     path('investor-register/', register_investor, name='InvestorRegister'),
@@ -7,6 +19,10 @@ urlpatterns = [
     path('business-register/', register_business, name='BusinessRegister'),
     path('business-login/', login_business, name='BusinessLogin'),
     path('invest/', invest, name='Invest'),
-    path('', ListInvestor.as_view()),
-    path('<int:pk>/', DetailInvestor.as_view()),
+    path('investor', ListInvestor.as_view()),
+    path('investor/<int:pk>/', DetailInvestor.as_view()),
+    path('business', ListBusiness.as_view()),
+    path('business/<int:pk>/', DetailBusiness.as_view()),
+    path('investment', ListInvestment.as_view()),
+    path('investment/<int:pk>/', DetailInvestment.as_view()),
 ]
