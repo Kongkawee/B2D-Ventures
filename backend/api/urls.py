4c9dd375bc17467f160255a613ed924aabe46c34
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-10-10 07:05:05 +0700

    Add invest path

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import register_investor, login_investor, register_business, login_business, ListInvestor, DetailInvestor
+from .views import register_investor, login_investor, register_business, login_business, invest, ListInvestor, DetailInvestor
 
 urlpatterns = [
     path('investor-register/', register_investor, name='InvestorRegister'),
     path('investor-login/', login_investor, name='InvestorLogin'),
     path('business-register/', register_business, name='BusinessRegister'),
     path('business-login/', login_business, name='BusinessLogin'),
+    path('invest/', invest, name='Invest'),
     path('', ListInvestor.as_view()),
     path('<int:pk>/', DetailInvestor.as_view()),
 ]
