1ad6002694014a0f9e8dbd6adb67b827538bfcce
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-10-14 12:00:13 +0700

    Fix urls routing

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,28 +1,19 @@
 from django.urls import path
-from .views import (
-    register_investor, 
-    login_investor, 
-    register_business, 
-    login_business,
-    invest,
-    ListInvestor, 
-    DetailInvestor,
-    ListBusiness,
-    DetailBusiness,
-    ListInvestment,
-    DetailInvestment,
-)
+from . import views
 
 urlpatterns = [
-    path('investor-register/', register_investor, name='InvestorRegister'),
-    path('investor-login/', login_investor, name='InvestorLogin'),
-    path('business-register/', register_business, name='BusinessRegister'),
-    path('business-login/', login_business, name='BusinessLogin'),
-    path('invest/', invest, name='Invest'),
-    path('investor', ListInvestor.as_view()),
-    path('investor/<int:pk>/', DetailInvestor.as_view()),
-    path('business', ListBusiness.as_view()),
-    path('business/<int:pk>/', DetailBusiness.as_view()),
-    path('investment', ListInvestment.as_view()),
-    path('investment/<int:pk>/', DetailInvestment.as_view()),
+    path('investor-login/', views.login_investor, name='InvestorLogin'),
+    path('business-register/', views.register_business, name='BusinessRegister'),
+    path('business-login/', views.login_business, name='BusinessLogin'),
+    path('invest/', views.invest, name='Invest'),
+    path('investor/', views.ListInvestor.as_view()),
+    path('investor/<int:pk>/', views.DetailInvestor.as_view()),
+    path('business/', views.ListBusiness.as_view()),
+    path('business/<int:pk>/', views.DetailBusiness.as_view()),
+    path('investment/', views.ListInvestment.as_view()),
+    path('investment/<int:pk>/', views.DetailInvestment.as_view()),
+    path('investment/investor/<int:pk>/', views.InvestmentByInvestorView.as_view(), name='investment-by-investor'),
+
+    path('investor/profile/', views.CurrentInvestorProfile.as_view(), name='CurrentInvestorProfile'),
+    path('investor/investment/', views.CurrentInvestorInvestment.as_view(), name='CurrentInvestorInvestment'),
 ]
