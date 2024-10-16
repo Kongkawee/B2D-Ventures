62dffdc7b8a5ed1feb44260a2626494d3794de93
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-10-16 17:45:51 +0700

    Update models and applied migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 
 
 class Investor(models.Model):
@@ -23,14 +24,20 @@ class Business(models.Model):
     phone_number = models.CharField(max_length=10, blank=False, null=False)
     publish_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    description = models.TextField()
+    cover_image = models.FileField(null=True, blank=True) # Remove null blank after tested
+    describe_images = models.FileField(null=True, blank=True) # Remove null blank after tested
+    fundraise_purpose = models.TextField(max_length=200)
+    brief_description = models.TextField(max_length=200)
+    pitch = models.JSONField(default=dict, null=True, blank=True) # Remove null blank after tested
+    business_category = models.JSONField(default=list, null=True, blank=True) # Remove null blank after tested
+    country_located = models.CharField(max_length=50, null=True, blank=True) # Remove null blank after tested
+    province_located = models.CharField(max_length=50, null=True, blank=True) # Remove null blank after tested
     goal = models.DecimalField(max_digits=12, decimal_places=2)
     min_investment = models.DecimalField(max_digits=12, decimal_places=2)
     max_investment = models.DecimalField(max_digits=12, decimal_places=2)
-    current_investment = models.DecimalField(max_digits=12, decimal_places=2)
+    current_investment = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     price_per_share = models.DecimalField(max_digits=12, decimal_places=2)
-    shares_detail = models.CharField(max_length=100)
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, default="pending")
 
     def __str__(self):
         return f"{self.company_name}: {self.business_name}"
