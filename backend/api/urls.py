d9dddc9ca3adc76e5614b2627453850d1c519434
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-10-16 17:47:06 +0700

    Update path for request, response and page routing

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,18 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('investor-login/', views.login_investor, name='InvestorLogin'),
-    path('business-register/', views.register_business, name='BusinessRegister'),
-    path('business-login/', views.login_business, name='BusinessLogin'),
-    path('invest/', views.invest, name='Invest'),
-    path('investor/', views.ListInvestor.as_view()),
-    path('investor/<int:pk>/', views.DetailInvestor.as_view()),
-    path('business/', views.ListBusiness.as_view()),
-    path('business/<int:pk>/', views.DetailBusiness.as_view()),
-    path('investment/', views.ListInvestment.as_view()),
-    path('investment/<int:pk>/', views.DetailInvestment.as_view()),
-    path('investment/investor/<int:pk>/', views.InvestmentByInvestorView.as_view(), name='investment-by-investor'),
+    path('invest/', views.invest, name='invest'),
+    path('investor/', views.ListInvestor.as_view(), name='all_investor'),
+    path('investor/<int:pk>/', views.DetailInvestor.as_view(), name='specific_investor'),
+    path('investor/profile/', views.CurrentInvestorProfile.as_view(), name='current_investor_profile'),
+    path('investor/investment/', views.CurrentInvestorInvestment.as_view(), name='current_investor_investment'),
 
-    path('investor/profile/', views.CurrentInvestorProfile.as_view(), name='CurrentInvestorProfile'),
-    path('investor/investment/', views.CurrentInvestorInvestment.as_view(), name='CurrentInvestorInvestment'),
+    path('business/', views.ListBusiness.as_view(), name='all_business'),
+    path('business/card/', views.ListAvailableBusinessForCard.as_view(), name='all_available_business_card'),
+    path('business/<int:pk>/', views.DetailBusiness.as_view(), name='specific_business'),
+    path('business/profile/', views.CurrentBusinessFundraise.as_view(), name='current_business_profile'),
+    path('business/fundraise/', views.CurrentBusinessFundraise.as_view(), name='current_business_fundraise'),
+
+    path('investment/', views.ListInvestment.as_view(), name='all_investment'),
+    path('investment/<int:pk>/', views.DetailInvestment.as_view(), name='specific_investment'),
+    path('investment/investor/<int:pk>/', views.InvestmentByInvestorView.as_view(), name='investment_by_investor'),
 ]
--- backend/b2d_ventures/urls.py
+++ backend/b2d_ventures/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import register_investor
+from api import views 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,9 +8,17 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/investor/register/', register_investor, name='investor_register'),
+
+    path('api/login/', views.login_user, name='investor_login'),
+
+    path('api/investor/register/', views.register_investor, name='investor_register'),
+
+    path('api/business/register/', views.register_business, name='business_register'),
+
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+
     path('api-auth/', include('rest_framework.urls')),
+
     path('api/', include('api.urls')),
 ]
