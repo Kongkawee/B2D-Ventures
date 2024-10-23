d6e379aecb5024042369518cdfe372672d450efd
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-10-23 18:18:10 +0700

    Update link to pop ups components

--- frontend/src/pages/checkout-page/components/RisksAcceptance.jsx
+++ frontend/src/pages/checkout-page/components/RisksAcceptance.jsx
@@ -1,22 +1,19 @@
 import * as React from 'react';
-
-import Divider from '@mui/material/Divider';
-import Grid from '@mui/material/Grid';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-
-const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
-const payments = [
-  { name: 'Card type:', detail: 'Visa' },
-  { name: 'Card holder:', detail: 'Mr. John Smith' },
-  { name: 'Card number:', detail: 'xxxx-xxxx-xxxx-1234' },
-  { name: 'Expiry date:', detail: '04/2024' },
-];
+import { useState } from 'react';
+import { Typography, Stack, Divider, Button } from '@mui/material';
+import PopUpRisks from '../../../components/PopUp/PopUpRisks';
 
 export default function RisksAcceptance() {
+  const [openRisksDialog, setOpenRisksDialog] = useState(false);
+
+  const handleOpenRisksDialog = () => {
+    setOpenRisksDialog(true);
+  };
+
+  const handleCloseRisksDialog = () => {
+    setOpenRisksDialog(false);
+  };
+
   return (
     <Stack spacing={2}>
       <Stack
@@ -30,104 +27,24 @@ export default function RisksAcceptance() {
             Risk Acceptance
           </Typography>
           <Typography variant="subtitle4" gutterBottom sx={{ color: 'text.secondary' }}>
-          By clicking "Next" you acknowledge that you have read, understood, and agree 
-          to the risks and disclaimers outlined. You accept that certain risks may arise 
-          in the future, and you agree to proceed with full awareness of these potential 
-          uncertainties.
+            By clicking "Next" you acknowledge that you have read, understood, and agree to the risks and disclaimers outlined. 
+            You accept that certain risks may arise in the future, and you agree to proceed with full awareness of these potential uncertainties.
           </Typography>
         </div>
+
         <div>
-          <Typography variant="subtitle1" gutterBottom>
-            Risks
-          </Typography>
-          <Grid container>
-            <React.Fragment>
-            <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: '100%', mb: 1 }}
-            >
-                <Typography variant="body1" sx={{ color: 'text.secondary' }}>
-                -
-                </Typography>
-                <Typography 
-                variant="body2" 
-                component="a"
-                href="/"
-                target="_blank"
-                sx={{
-                    textDecoration: 'none',
-                    color: '#1976d2',
-                    cursor: 'pointer',
-                    '&:hover': {
-                      textDecoration: 'underline',
-                    },
-                  }}
-                >
-                Risk from changes of exchange rate
-                </Typography>
-            </Stack>
-            </React.Fragment>
-            <React.Fragment>
-            <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: '100%', mb: 1 }}
-            >
-                <Typography variant="body1" sx={{ color: 'text.secondary' }}>
-                -
-                </Typography>
-                <Typography 
-                variant="body2" 
-                component="a"
-                href="/"
-                target="_blank"
-                sx={{
-                    textDecoration: 'none',
-                    color: '#1976d2',
-                    cursor: 'pointer',
-                    '&:hover': {
-                      textDecoration: 'underline',
-                    },
-                  }}
-                >
-                Risk from transaction charge
-                </Typography>
-            </Stack>
-            </React.Fragment>
-            <React.Fragment>
-            <Stack
-                direction="row"
-                spacing={1}
-                useFlexGap
-                sx={{ width: '100%', mb: 1 }}
-            >
-                <Typography variant="body1" sx={{ color: 'text.secondary' }}>
-                -
-                </Typography>
-                <Typography 
-                variant="body2" 
-                component="a"
-                href="/"
-                target="_blank"
-                sx={{
-                    textDecoration: 'none',
-                    color: '#1976d2',
-                    cursor: 'pointer',
-                    '&:hover': {
-                      textDecoration: 'underline',
-                    },
-                  }}
-                >
-                Risk from business
-                </Typography>
-            </Stack>
-            </React.Fragment>
-          </Grid>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleOpenRisksDialog}
+            sx={{ mt: 2 }}
+          >
+            View Risks
+          </Button>
         </div>
       </Stack>
+
+      <PopUpRisks open={openRisksDialog} handleClose={handleCloseRisksDialog} />
     </Stack>
   );
 }
