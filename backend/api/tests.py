9d21643859f74d91fd8450e3f159a2e80b1a7af5
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-10-28 12:28:01 +0700

    Write test for login

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -78,4 +78,28 @@ class BusinessRegistrationTests(APITestCase):
         self.assertIn("access", response.data)
 
 
+class LoginTests(UserSetupMixin, APITestCase):
+    def test_login_investor(self):
+        url = reverse('investor_login')
+        data = {
+            "username": "investor_user",
+            "password": "password123",
+            "type": "investor"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["role"], "investor")
+
+    def test_login_business(self):
+        url = reverse('investor_login')
+        data = {
+            "username": "business_user",
+            "password": "password123",
+            "type": "business"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["role"], "business")
+
+
 
