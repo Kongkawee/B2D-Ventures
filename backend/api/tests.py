2c6d16bd9ab2c048a56f50b8b7131e54b25fe6aa
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-10-28 12:29:09 +0700

    Write test for investment list

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -173,3 +173,28 @@ class ProfileTests(UserSetupMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['email'], self.business.email)
 
+class InvestmentListTests(UserSetupMixin, APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.investor_token = str(RefreshToken.for_user(self.investor_user).access_token)
+        self.business_token = str(RefreshToken.for_user(self.business_user).access_token)
+        self.investment = Investment.objects.create(
+            investor=self.investor,
+            business=self.business,
+            amount=1000,
+            shares=20
+        )
+
+    def test_list_investments_by_investor(self):
+        url = reverse('current_investor_investment')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.investor_token}')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_list_investments_by_business(self):
+        url = reverse('current_business_fundraise')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.business_token}')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
