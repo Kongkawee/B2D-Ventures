af0a78b3e1ad331d5a934f949ca970dc79982836
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-10-28 12:26:44 +0700

    Write test for investor registration

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -38,3 +38,20 @@ class UserSetupMixin:
             current_investment=98000,
             status="pending"
         )
+
+class InvestorRegistrationTests(APITestCase):
+    def test_register_investor(self):
+        url = reverse('investor_register')
+        data = {
+            "username": "new_investor",
+            "email": "new_investor@example.com",
+            "password": "newpassword123",
+            "firstName": "New",
+            "lastName": "Investor",
+            "phoneNumber": "1234567890"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn("access", response.data)
+
+
