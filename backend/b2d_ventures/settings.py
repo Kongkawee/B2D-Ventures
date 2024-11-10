9cfe02d524fb29d553533629fae2ded45e417f71
Kongkawee CHAYARAT <armkongkawee@gmail.com>
2024-11-10 19:21:32 +0700

    Update settings.py

--- backend/b2d_ventures/settings.py
+++ backend/b2d_ventures/settings.py
@@ -97,7 +97,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'b2d_ventures.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -176,8 +175,19 @@ else:
     AWS_SECRET_ACCESS_KEY = getenv('SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = getenv('BUCKET_NAME')
     AWS_S3_REGION_NAME = getenv('S3_REGION_NAME')
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+}
