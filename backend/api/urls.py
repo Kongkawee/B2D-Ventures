e5b1635c8a1f5ad984293b8aa16251b33b819acc
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-11-19 21:42:13 +0700

    Add path for update investor

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,16 +8,20 @@ urlpatterns = [
     path('investor/<int:pk>/', DetailInvestor.as_view(), name='specific_investor'),
     path('investor/profile/', CurrentInvestorProfile.as_view(), name='current_investor_profile'),
     path('investor/investment/', CurrentInvestorInvestment.as_view(), name='current_investor_investment'),
+    path('investor/update/', UpdateInvestor.as_view(), name='update_investor'),
+
 
     path('business/', ListBusiness.as_view(), name='all_business'),
     path('business/card/', ListAvailableBusinessForCard.as_view(), name='all_available_business_card'),
     path('business/<int:pk>/', DetailBusiness.as_view(), name='specific_business'),
     path('business/profile/', CurrentBusinessProfile.as_view(), name='current_business_profile'),
     path('business/fundraise/', CurrentBusinessFundraise.as_view(), name='current_business_fundraise'),
+    path('business/<int:business_id>/upload_images/', upload_business_images, name='upload_business_images'),
+
 
     path('investment/', ListInvestment.as_view(), name='all_investment'),
     path('investment/<int:pk>/', DetailInvestment.as_view(), name='specific_investment'),
     path('investment/investor/<int:pk>/', InvestmentByInvestorView.as_view(), name='investment_by_investor'),
-    
-    path('business/<int:business_id>/upload_images/', upload_business_images, name='upload_business_images'),
+    path('investment/business/<int:business_id>/', InvestmentByBusinessView.as_view(), name='investment-by-business'),
+
 ]
