340815c64324106a2c8ce8258c9f60c5e2e54f41
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-11-21 23:12:32 +0700

    Add password reset model

---
+++ backend/api/models/password_reset.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+
+class PasswordReset(models.Model):
+    """PasswordReset represents the field needed to store Token for password resetting"""
+    email = models.EmailField(blank=False, null=False)
+    token = models.CharField(max_length=100)
+    used = models.BooleanField(default=False)
+    expire_date = models.DateTimeField(null=True)
+    
