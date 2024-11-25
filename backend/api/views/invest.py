97d52d414f2692b78465fa61c5700591285308b9
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-11-25 18:54:06 +0700

    Add logic to check for if previois investments exceed the max amount

--- backend/api/views/invest.py
+++ backend/api/views/invest.py
@@ -1,5 +1,6 @@
 from ..serializers import InvestmentSerializer
 from ..models import Business, Investor, Investment
+from django.db import models
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
@@ -18,7 +19,6 @@ def invest(request):
 
     business_id = request.data.get('business_id')
     amount = request.data.get('amount')
-    shares = request.data.get('shares')
 
     if not investor_id or not business_id or not amount:
         return Response({'error': 'Missing required fields.'}, status=status.HTTP_400_BAD_REQUEST)
@@ -32,13 +32,21 @@ def invest(request):
         business = Business.objects.get(id=business_id)
     except Business.DoesNotExist:
         return Response({'error': 'Business not found.'}, status=status.HTTP_404_NOT_FOUND)
+    
+    previous_investment_total = Investment.objects.filter(
+        investor=investor,
+        business=business
+    ).aggregate(total=models.Sum('amount'))['total'] or 0
 
     if amount <= 0:
         return Response({'error': 'Investment amount must be positive.'}, status=status.HTTP_400_BAD_REQUEST)
 
     if amount < business.min_investment or amount > business.max_investment:
         return Response({'error': f'Investment must be between {business.min_investment} and {business.max_investment}.'}, status=status.HTTP_400_BAD_REQUEST)
-
+    
+    if previous_investment_total + amount > business.max_investment:
+        return Response({'error': 'Total investment by you exceeds the allowed maximum investment.'}, status=status.HTTP_400_BAD_REQUEST)
+    
     if business.current_investment + amount > business.goal:
         return Response({'error': 'Investment exceeds the business goal.'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -49,7 +57,7 @@ def invest(request):
         investor=investor,
         business=business,
         amount=amount,
-        shares=shares
+        shares= amount / (business.goal / business.stock_amount)
     )
 
     investment_serializer = InvestmentSerializer(investment)
