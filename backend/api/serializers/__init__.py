40742af527f51eca82b52cdbf0b12803ae10be02
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-11-25 18:53:37 +0700

    Remove unused Serializer

--- backend/api/serializers/__init__.py
+++ backend/api/serializers/__init__.py
@@ -1,7 +1,6 @@
 from .business_serializer import BusinessSerializer
 from .investor_serializer import InvestorSerializer
 from .investment_serializer import InvestmentSerializer
-from .business_card_serializer import BusinessCardSerializer
 from .business_image_serializer import BusinessImageSerializer
 from .reset_password_request_serializer import ResetPasswordRequestSerializer
 from .password_reset_serializer import ResetPasswordSerializer
