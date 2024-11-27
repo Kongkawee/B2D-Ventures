3b21f5366e6760a3659bbd492e96c8e96aff0393
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-11-27 22:46:02 +0700

    Add BusinessDoc Model

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -1,4 +1,4 @@
-from .business import Business, BusinessImage
+from .business import Business, BusinessImage, BusinessDocument
 from .investment import Investment
 from .investor import Investor
 from .password_reset import PasswordReset
