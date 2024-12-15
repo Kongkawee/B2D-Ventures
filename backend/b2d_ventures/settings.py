dfc4ff4147d0651a2d3a1cc3b90d2ceef9aa2c0e
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-12-16 02:27:37 +0700

    CORS update

--- backend/b2d_ventures/settings.py
+++ backend/b2d_ventures/settings.py
@@ -168,7 +168,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = getenv('CORS_ALLOWED_ORIGINS').split(",")
 CORS_ALLOWS_CREDENTIALS = True
 
 # CSRF_COOKIE_NAME = "csrftoken"
